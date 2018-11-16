990c17f2da5914d718ae415636eb6d211fcb7833
Christian López Barrón <chris.gfz@gmail.com>
2018-11-16 23:03:03 +1100

    version up

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='grill-names',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(
         exclude=("*.tests", "*.tests.*", "tests.*", "tests", "*.docs", "*.docs.*", "docs.*", "docs")),
     description='Name objects for digital content creation.',
     author='Christian López Barrón',
     author_email='chris.gfz@gmail.com',
     url='https://github.com/thegrill/grill-names',
-    download_url='https://github.com/thegrill/grill-names/releases/tag/1.0.0',
+    download_url='https://github.com/thegrill/grill-names/releases/tag/1.1.0',
     classifiers=['Programming Language :: Python :: 3.7'],
     extras_require={'docs': ['sphinx_autodoc_typehints', 'sphinx_rtd_theme']},
     install_requires=['naming'],
