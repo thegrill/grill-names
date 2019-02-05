d76c954683caea3afdc0ce86af229be0aaeabf8b
Christian López Barrón <chris.gfz@gmail.com>
2019-02-05 23:05:40 +1100

    ids as config files. Added LifeTR.

--- grill/ids/__init__.py
+++ grill/ids/__init__.py
@@ -1,1 +1,21 @@
-from . import cgasset
+import configparser
+from importlib import resources
+from functools import lru_cache
+from types import MappingProxyType
+
+
+@lru_cache()
+def __getattr__(name):
+    cfg_fname = f'{name}.cfg'
+    if resources.is_resource(__name__, cfg_fname):
+        cfg = configparser.ConfigParser({'default': '', 'pattern': '\w+'})
+        cfg.read_string(resources.read_text(__name__, cfg_fname))
+        return MappingProxyType({s: MappingProxyType(dict(cfg[s])) for s in cfg.sections()})
+    raise AttributeError(f"module {__name__} has no attribute {name}")
+
+
+def __dir__():
+    return tuple(
+        # how to do better?
+        c.split('.cfg')[0] for c in resources.contents(__name__) if c.endswith('.cfg')
+    )
--- grill/ids/tests/tests.py
+++ grill/ids/tests/tests.py
@@ -5,13 +5,11 @@ from grill import ids
 class TestIDs(unittest.TestCase):
 
     def test_keys(self):
-        contents = dir(ids.cgasset)
-        for k in ids.cgasset.IDS:
-            res = getattr(ids.cgasset, k)
+        for k in dir(ids):
+            res = getattr(ids, k)
             with self.assertRaises(TypeError):
                 res[k] = 'sorry'
-            self.assertTrue(k in contents)
 
     def test_mod(self):
         with self.assertRaises(AttributeError):
-            ids.cgasset.hello
+            ids.hello
