c4bbf49ceb1f6a7f9040a4af0706f0dfd8343f1a
Christian López Barrón <chris.gfz@gmail.com>
2019-10-24 02:21:46 +1100

    use find_namespace_packages in setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='grill-names',
-    version='1.4.0',
-    packages=find_packages(
+    version='1.4.1',
+    packages=find_namespace_packages(
         exclude=("*.tests", "*.tests.*", "tests.*", "tests", "*.docs", "*.docs.*", "docs.*", "docs")),
     package_data={
         'grill.tokens.ids': ['*.cfg'],
@@ -13,7 +13,7 @@ setup(
     author='Christian López Barrón',
     author_email='chris.gfz@gmail.com',
     url='https://github.com/thegrill/grill-names',
-    download_url='https://github.com/thegrill/grill-names/releases/tag/1.4.0',
+    download_url='https://github.com/thegrill/grill-names/releases/tag/1.4.1',
     classifiers=['Programming Language :: Python :: 3.7'],
     extras_require={'docs': ['sphinx_autodoc_typehints', 'sphinx_rtd_theme']},
     install_requires=['naming'],
