a584fbf740678e7c65aa40fc0ea7b92a0b154ec4
chrizzftd <chris.gfz@gmail.com>
2020-07-14 19:08:11 +1000

    Renamed branch to stream (#17)
    * renamed branch to stream
    
    * lead instead of master on stream
    
    * remove conda build on docs
    
    * use setup.cfg

--- setup.py
+++ setup.py
@@ -1,25 +1,2 @@
-#!/usr/bin/env python
-from setuptools import setup, find_namespace_packages
-
-_VERSION = '1.5.3'
-setup(
-    name='grill-names',
-    version=_VERSION,
-    packages=find_namespace_packages(
-        exclude=("*.tests", "*.tests.*", "tests.*", "tests", "*.docs", "*.docs.*", "docs.*", "docs")),
-    package_data={
-        'grill.tokens.ids': ['*.cfg'],
-    },
-    description='Name objects for digital content creation.',
-    author='Christian López Barrón',
-    author_email='chris.gfz@gmail.com',
-    url='https://github.com/thegrill/grill-names',
-    download_url=f'https://github.com/thegrill/grill-names/releases/tag/{_VERSION}',
-    classifiers=[
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    extras_require={'docs': ['sphinx_autodoc_typehints', 'sphinx_rtd_theme']},
-    install_requires=['naming>=0.5'],
-    namespace_packages=['grill']
-)
+import setuptools
+setuptools.setup()
