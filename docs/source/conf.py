99b390831401bb16b7d97d5726b4a4c6cc359988
Christian López Barrón <chris.gfz@gmail.com>
2020-12-30 15:16:06 +1100

    Docs - Inheritance Diagram Links (#21)
    * py3.9 and grill sphinx fix for intersphinx inheritance diagram

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -196,5 +196,5 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 # intersphinx_mapping = {'https://docs.python.org/': None}
-intersphinx_mapping = {'python': ('https://docs.python.org/3.8', None),
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None),
                        'naming': ('http://naming.readthedocs.io/en/latest/', None)}
