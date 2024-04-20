70f7bb24aee6c44944023d37a34091facf67f750
Christian López Barrón <chris.gfz@gmail.com>
2024-04-20 17:02:37 +1000

    modern css declaration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -139,9 +139,11 @@ html_theme = 'shibuya'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
-def setup(app):
-   app.add_css_file("custom.css")
-
+# def setup(app):
+#    app.add_css_file("custom.css")
+html_css_files = [
+  'custom.css',
+]
 
 # -- Options for HTMLHelp output ------------------------------------------
 
