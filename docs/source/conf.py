74cd24d32e8a6eda6ea57759913ef6126282e682
Christian López Barrón <chris.gfz@gmail.com>
2024-04-20 16:41:55 +1000

    test custom css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -139,6 +139,8 @@ html_theme = 'shibuya'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+def setup(app):
+   app.add_css_file("custom.css")
 
 
 # -- Options for HTMLHelp output ------------------------------------------
